b2e918a6eb16c95ccfe09ef852935009de69147b
GOLF <golfyos@GOLF-Macbook.local>
2017-11-30 23:35:04 +0700

    add delete

--- routes/server.js
+++ routes/server.js
@@ -3,7 +3,7 @@ import Player from '../models/player'
 
 const router = express.Router()
 
-router.get("/getdata",(req,res)=>{
+router.get("/getdata",(req,res,next)=>{
     Player.find()
         .exec()
         .then(result=>{
@@ -15,7 +15,7 @@ router.get("/getdata",(req,res)=>{
 
 })
 
-router.post("/addplayer",(req,res)=>{
+router.post("/addplayer",(req,res,next)=>{
     const data = new Player({
         name : req.body.name,
         move : req.body.move,
@@ -26,9 +26,50 @@ router.post("/addplayer",(req,res)=>{
             if(!result) res.send({status:-1,msg:"cannot add player to board"})
             else res.send({status:0,msg:"added to ranking board"})
         })
-        .catch(err=>{
-            next(err)
-        })
+        .catch(err=>next(err))
 })
+
+router.get("/private/deletedata/:id",(req,res,next)=>{
+    const id = req.params.id
+    let dataObj = getDeletedData(id)
+    if(dataObj!=="nu"){
+        dataObj.then(data=>{
+                if(id==="all"){
+                    Player.remove({})
+                        .then(result=>{
+                            res.send({status:0,msg:"Deleted All"})
+                        })
+                }else{
+                    Player.remove({_id:id})
+                        .then(result=>{
+                            res.send({status:0,msg:`${data} Deleted`})
+                        })
+                        .catch(err=>next(err))
+                }
+            })      
+    }else{
+        dataObj
+            .then(err=>{
+                console.log(err)
+                res.send({status:0,msg:"No data to delete"})
+
+            })
+    }
+})
+
+const getDeletedData = async id =>{
+    let temp = await new Promise((resolve,reject)=>{
+        Player.findOne({_id:id})
+        .then(result=>{
+            if(result)
+                resolve(result)
+            else
+                return reject("nu")
+        })
+        .catch(err=>resolve(err))
+    })
+   
+    return temp
+}
 
 export default router
