50c9392e300bbb4fcfef28d5327050fb07adc22d
GOLF <golf_cat1@hotmail.com>
2017-12-08 00:22:20 +0700

    test

--- config/database.js
+++ config/database.js
@@ -2,5 +2,4 @@ module.exports = {
     database : "mongodb://4c7e47ab7a2ecb9871b491e0d9977f73:admin@5a.mongo.evennode.com:27017,5b.mongo.evennode.com:27017/4c7e47ab7a2ecb9871b491e0d9977f73?replicaSet=us-5",
     // database : "http://mongodb-matchcard.193b.starter-ca-central-1.openshiftapps.com:27017/matchcard",
     secret : "yoursecret"
-    
 }
